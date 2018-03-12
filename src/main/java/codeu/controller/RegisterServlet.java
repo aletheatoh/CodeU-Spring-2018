9dbbed3dc084ab030f3701efb3e7713bd27f0455
erinyoo <erinyoo@ufl.edu>
2018-03-12 12:31:51 -0400

    Fixed login.jsp and register servlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -11,6 +11,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RegisterServlet extends HttpServlet {
 
+	/**
+	 * Store class that gives access to Users.
+	 */
+	private UserStore userStore;
+
 	@Override
 	public void doGet (HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
@@ -25,4 +30,22 @@ public class RegisterServlet extends HttpServlet {
 		response.getWriter().println("<p>Username: " + username + "</p>");
 		response.getWriter().println("<p>Password: " + password + "</p>");
 	}
+
+	/**
+     * Set up state for handling registration-related requests. This method is only called when
+	 * running in a server, not when running in a test.
+	 */
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		setUserStore(UserStore.getInstance());
+	}
+
+	/**
+	 * Sets the UserStore used by the servlet. THis function provides a common setup method
+	 * for use by the test framework or the servlet's init() function.
+	 */
+	 void setUserStore(UserStore userStore) {
+		 this.userStore = userStore;
+	 }
 }
