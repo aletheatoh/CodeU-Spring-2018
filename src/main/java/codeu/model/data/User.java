b141cc396df596d8487405d82c31f106a1aa70c4
Alethea Toh <alethea.toh@duke.edu>
2018-04-16 21:51:25 +0800

    Update User.java

--- src/main/java/codeu/model/data/User.java
+++ src/main/java/codeu/model/data/User.java
@@ -19,12 +19,12 @@ import java.util.UUID;
 
 /** Class representing a registered user. */
 public class User {
-  // removed all the final keywords
+  // only allow user to update password, aboutMe, and profilePicture
   private final UUID id;
   private final String name;
   private final Instant creation;
+  
   private String hashedPassword;
-
   private String aboutMe;
   private String profilePicture;
 
@@ -78,6 +78,7 @@ public class User {
     return profilePicture;
   }
 
+  // only allow user to update password, aboutMe, and profilePicture
   public void updateUser(String password, String aboutMe, String profilePicture) {
     this.hashedPassword = password;
     this.aboutMe = aboutMe;
